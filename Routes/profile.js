841b8495ecf56350fccce290729bc2b80b2a4807
CaroD59 <dorchies.c@gmail.com>
2021-06-28 15:27:22 +0200

    Routes added

---
+++ Routes/profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const { db } = require('../db-config');
+
+router.get('/', async (req, res) => {
+  const sql = 'SELECT * from profile';
+  const [results] = await db.query(sql);
+  res.json(results);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const homeRoute = require('./Routes/profile');
+const profileRoute = require('./Routes/profiledescritpion');
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
-});
+const PORT = process.env.PORT || 8000;
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
-});
+app.use(express.json());
+app.use(cors());
+app.use('/', homeRoute);
+app.use('/profile', profileRoute);
 
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
