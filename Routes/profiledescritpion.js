d2f93b0332ca1b23144b20fbd031ec47a3527c6d
CaroD59 <dorchies.c@gmail.com>
2021-06-29 09:04:43 +0200

    errors fixed

--- Routes/profiledescritpion.js
+++ Routes/profiledescritpion.js
@@ -3,23 +3,25 @@ const express = require('express');
 const router = express.Router();
 const { db } = require('../db-config');
 
-router.get('/', async (req, res) => {
-  const sql = 'SELECT * from profile';
-  const [results] = await db.query(sql);
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT * FROM profile WHERE id=?';
+  const sqlValues = [id];
+  const [results] = await db.query(sql, sqlValues);
   res.json(results);
 });
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const [rows] = await db.query(
-    'SELECT content, author, datetime FROM comments'
+    'SELECT content, author, datetime FROM comments WHERE id=?'
   );
   res.status(200).json(rows);
 });
 
-router.post('/', async (req, res) => {
+router.post('/:id', async (req, res) => {
   const { author, content } = req.body;
   await db.query(
-    'INSERT INTO messages(author, datetime, content) VALUES(?,?,?)',
+    'INSERT INTO comments(author, datetime, content) VALUES(?,?,?) WHERE id=?',
     [author, new Date(), content]
   );
   res.status(201).send(`Received new message: ${author} said ${content}`);
